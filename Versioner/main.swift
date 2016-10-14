18ca298c46d9daffcbe2c4d1735a669a87dd4c29
Luc-Olivier Lafeuille <lo.lafeuille@asity-intl.com>
2016-10-14 11:08:52 +0200

    Initial Commit

--- Versioner/main.swift
+++ Versioner/main.swift
@@ -2,11 +2,90 @@
 //  main.swift
 //  Versioner
 //
-//  Created by Luc-Olivier on 10/14/16.
+//  Created by Luc-Olivier on 9/27/16.
 //  Copyright © 2016 Luc-Olivier. All rights reserved.
 //
 
 import Foundation
 
-print("Hello, World!")
+let debug = 1
 
+/* Static Params */
+
+let files = [
+    ("src/js/config/config.js","", "version:"),
+    ("config.xml", "pokedesk", "version=")
+]
+
+/* Funcs */
+
+func usage() {
+    print ("Usage: \(basename()) version-number [root path]")
+    exit(1)
+}
+
+func errExit(_ message: String) {
+    print ("\(basename()) error: \(message)")
+    exit(2)
+}
+
+/* Preset */
+
+let argAmt = CommandLine.arguments.count
+if (argAmt < 2 || argAmt > 3) { usage() }
+let replString = CommandLine.arguments[1]
+var rootPath = ""
+if (argAmt == 3) {
+    rootPath = CommandLine.arguments[2]
+    if (rootPath[rootPath.startIndex] != "/") {
+        rootPath = currentDir() + "/" + rootPath
+    }
+    if (rootPath[rootPath.index(before: rootPath.endIndex)] != "/") {
+        rootPath += "/"
+    }
+    if (!directoryExists(rootPath)) { errExit("Root path not found \(rootPath)") }
+}
+if debug > 0 {
+    print("#replString: \(replString)")
+    print("#rootPath:   \(rootPath)")
+}
+
+/* Checkup */
+for (fileName, selector, niddle) in files {
+    let filePath=rootPath+fileName
+    if debug > 0 {
+        print("#fileName: \(fileName)")
+        print("#selector: \(selector)")
+        print("#niddle: \(niddle)")
+        print("#filePath: \(filePath)")
+    }
+    if (!fileExists(filePath)) { errExit("\(filePath) not found!") }
+    
+    print ("------------")
+    if let file = File(path: filePath) {
+        if file.open() {
+            while true {
+                if let line = file.nextLine() {
+                    print(line)
+                } else {
+                    break
+                }
+            }
+        }
+    }
+    
+    if (selector != "") {
+        
+    } else {
+        
+    }
+}
+
+
+
+
+/*
+ for argument in CommandLine.arguments {
+ print(argument)
+ }
+ */
---
+++ Versioner/tools.swift
@@ -0,0 +1,108 @@
+//
+//  tools.swift
+//  Versioner
+//
+//  Created by Luc-Olivier on 9/29/16.
+//  Copyright © 2016 Luc-Olivier. All rights reserved.
+//
+
+import Foundation
+
+
+func basename() -> String {
+    return (CommandLine.arguments[0] as NSString).lastPathComponent
+}
+
+func currentDir() -> String {
+    let fileManager = FileManager.default
+    return fileManager.currentDirectoryPath
+}
+
+enum PathType { case isDirectory, isFile, notExists }
+
+func checkPath(_ path: String) -> PathType {
+    let fileManager = FileManager.default
+    var isDir: ObjCBool = false
+    fileManager.fileExists(atPath: path, isDirectory: &isDir)
+    if (isDir.boolValue) { return .isDirectory }
+    if (fileManager.fileExists(atPath: path)) { return .isFile }
+    return .notExists
+}
+
+func directoryExists(_ path: String) -> Bool {
+    return (checkPath(path) == .isDirectory) ? true : false
+}
+func fileExists(_ path: String) -> Bool {
+    return (checkPath(path) == .isFile) ? true : false
+}
+
+//func cat(_ path: String) ->
+
+class File {
+    var filepath: String!
+    
+    // constants
+    let delimiter = "\n"
+    let encoding = String.Encoding.utf8
+    let chunkSize = 4096
+    
+    // convenient vars
+    var hook: FileHandle!
+    let encodedDelimiter: Data
+    var buffer: Data
+    var eof: Bool = false
+    
+    init?(path: String) {
+        guard fileExists(path) else { return nil }
+        self.filepath = path
+        self.encodedDelimiter =  "\n".data(using: encoding)!
+        self.buffer = Data(capacity: chunkSize)
+    }
+    
+    deinit { self.close() }
+    
+    func open() -> Bool {
+        self.hook = FileHandle(forReadingAtPath: self.filepath)
+        return hook != nil ? true : false
+    }
+    
+    func close() {
+        hook?.closeFile()
+        hook = nil
+    }
+    
+    func nextLine() -> String? {
+        precondition(hook != nil, "Closed file")
+        
+        if eof { return nil }
+        
+        while !eof {
+            // get the range of next delimiter occurence
+            if let range = buffer.range(of: encodedDelimiter) {
+                // Convert complete line from buffer
+                let line = String(data: buffer.subdata(in: 0..<range.lowerBound), encoding: self.encoding)
+                // Remove line from buffer
+                buffer.removeSubrange(0..<range.upperBound)
+                //
+                return line
+            }
+            // get the next chunk into the buffer
+            let interBuffer = hook.readData(ofLength: self.chunkSize)
+            if interBuffer.count > 0 {
+                buffer.append(interBuffer)
+            } else {
+                // eof or reading issue
+                eof = true
+                // let see if buffer countain last line
+                if buffer.count > 0 {
+                    let line = String(data: buffer as Data, encoding: encoding)
+                    buffer.count = 0
+                    return line
+                }
+            }
+        }
+        return nil
+    }
+    
+}
+
