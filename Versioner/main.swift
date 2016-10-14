ab1e6b0a2e404365a618f12cbb63d77eb1c2adf4
Luc-Olivier Lafeuille <lo.lafeuille@asity-intl.com>
2016-10-14 11:02:27 +0200

    Initial Commit

---
+++ Versioner/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Versioner
+//
+//  Created by Luc-Olivier on 10/14/16.
+//  Copyright © 2016 Luc-Olivier. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
